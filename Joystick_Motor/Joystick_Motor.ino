419414e26f94fc495be87450bdfd74ecd4a1d280
amanueln328 <amanueln328@gmail.com>
2026-09-02 15:59:57 -0700

    Testing joystick

--- Joystick_Motor/Joystick_Motor.ino
+++ Joystick_Motor/Joystick_Motor.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+int xyzPins[] = {28, 27, 26}; // x, y, and z pins in that order
 
+void setup() {
+  Serial.begin(115200);
+  pinMode(xyzPins[2], INPUT_PULLUP); // z axis is a button
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  printJoystickVals();
+}
 
+void printJoystickVals() {
+  int xVal = analogRead(xyzPins[0]);
+  int yVal = analogRead(xyzPins[1]);
+  int zVal = digitalRead(xyzPins[2]);
+
+  Serial.println("X,Y,Z: " + String(xVal) + ", " + String(yVal) + ", " + String(zVal));
+  delay(500);
 }
